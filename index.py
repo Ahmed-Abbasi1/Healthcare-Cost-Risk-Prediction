a6094e0290c09572174259735929e7ea232a1921
Ahmed-Abbasi1 <ahmedabassi093@gmail.com>
2026-01-13 14:08:32 +0500

    cleaning

--- index.py
+++
@@ -1,8 +0,0 @@
-"""
-Vercel Entry Point for Healthcare Cost Prediction API
-This file is required by Vercel to identify the FastAPI application
-"""
-from app.api import app
-
-# Vercel expects an 'app' or 'handler' variable
-handler = app
